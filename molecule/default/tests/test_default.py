65be10df5909871a08a5d58e1de3fc19135cc8e9
Maros Scislak <maros.scislak@smilingwords.eu>
2020-03-08 14:26:28 +0100

    Add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_cfssl_binaries(host):
+    binaries = [
+        "cfssl",
+        "cfssljson",
+        "cfssl-bundle",
+        "cfssl-certinfo",
+        "cfssl-newkey",
+        "cfssl-scan",
+        "mkbundle",
+        "multirootca"
+    ]
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    for binary in binaries:
+        binary_file = host.file('/usr/local/bin/' + binary)
+        assert binary_file.exists
+        assert binary_file.user == 'root'
+        assert binary_file.group == 'root'
+        assert binary_file.mode == 0o755
